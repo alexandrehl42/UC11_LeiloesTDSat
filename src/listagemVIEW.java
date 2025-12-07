e8a24835cde5f6759c1b058b84eaea2f0d38332e
alexandrehl42 <alexandre.libera@gmail.com>
2025-12-07 20:27:06 -0300

    Implementa navegação para vendasVIEW

--- src/listagemVIEW.java
+++ src/listagemVIEW.java
@@ -156,6 +156,7 @@ public class listagemVIEW extends javax.swing.JFrame {
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
         //vendasVIEW vendas = new vendasVIEW(); 
         //vendas.setVisible(true);
+        new vendasVIEW().setVisible(true);
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
